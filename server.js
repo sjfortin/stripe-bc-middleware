957b259a14ea659aeb875ae4005d460b9cc6a82f
Sam Fortin <sam.fortin@irishtitan.com>
2023-05-18 16:54:17 -0500

    webhook is firing

--- server.js
+++ server.js
@@ -1,11 +1,17 @@
+require('dotenv').config();
 const express = require("express");
 const bodyParser = require("body-parser");
 const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
+const fs = require("fs");
 
 const app = express();
 
 // Stripe requires the raw body to construct the event.
 app.use(bodyParser.raw({ type: "application/json" }));
+
+app.get("/", (req, res) => {
+  res.send("Webhook party time!");
+});
 
 app.post("/webhook", (req, res) => {
   let event;
@@ -17,6 +23,7 @@ app.post("/webhook", (req, res) => {
       process.env.STRIPE_WEBHOOK_SECRET
     );
   } catch (err) {
+    fs.appendFileSync("log.txt", `Webhook Error: ${err.message}\n`);
     return res.status(400).send(`Webhook Error: ${err.message}`);
   }
 
@@ -24,9 +31,14 @@ app.post("/webhook", (req, res) => {
   switch (event.type) {
     case "payment_intent.succeeded":
       const paymentIntent = event.data.object;
+      fs.appendFileSync(
+        "log.txt",
+        `PaymentIntent was successful: ${JSON.stringify(paymentIntent)}\n`
+      );
       console.log(`PaymentIntent was successful!`);
       break;
     default:
+      fs.appendFileSync("log.txt", `Unhandled event type ${event.type}.\n`);
       console.log(`Unhandled event type ${event.type}.`);
   }
 
